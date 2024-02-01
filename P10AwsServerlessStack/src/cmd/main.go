6840776b23349e1f6fb3e2a09b7c045cfd22072a
kojoluh <dordoedelivery@gmail.com>
2024-02-01 21:56:29 +0000

    fixed syntax and typo precompile issues/bugs

--- P10AwsServerlessStack/src/cmd/main.go
+++ P10AwsServerlessStack/src/cmd/main.go
@@ -1,43 +1,46 @@
 package main
 
 import (
+	"aws-serverless-stack/src/pkg/handlers"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	dynaClient dynamodbiface.DynamoDBAPI
 )
 
 func main() {
 	region := os.Getenv("AWS_REGION")
 	awsSession, err := session.NewSession(&aws.Config{
-		Region: aws.String(region),
-	})
+		Region: aws.String(region)})
 
 	if err != nil {
 		return
 	}
-
 	dynaClient = dynamodb.New(awsSession)
 	lambda.Start(handler)
 }
 
-const tableName = "LambdaInGoUser"
+const tableName = "goaws-serverless-stack"
 
 func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	switch req.HTTPMethod {
 	case "GET":
 		return handlers.GetUser(req, tableName, dynaClient)
 	case "POST":
-		return handlers.GetUser(req, tableName, dynaClient)
+		return handlers.CreateUser(req, tableName, dynaClient)
 	case "PUT":
-		return handlers.GetUser(req, tableName, dynaClient)
+		return handlers.UpdateUser(req, tableName, dynaClient)
 	case "DELETE":
-		return handlers.GetUser(req, tableName, dynaClient)
-	
-	}
+		return handlers.DeleteUser(req, tableName, dynaClient)
 	default:
-		return handlers.GetUser(req, tableName, dynaClient)
+		return handlers.UnhandledMethod()
+	}
 }
